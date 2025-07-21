299272177341fb57d54b5375c16cc20a8344f952
andrewleachtx <andrewleach38@gmail.com>
2025-07-21 15:35:48 -0500

    Add outline for commit command and loadConfig, added init overwrite option with backup logic

--- vit/cli.py
+++ vit/cli.py
@@ -1,16 +1,21 @@
+import subprocess
+from git import Repo
 import typer
-from vit.config import initConfig
+from vit.config import initConfig, loadConfig
+from pathlib import Path
 
 app = typer.Typer()
 
 @app.command()
-def init():
+def init(
+    overwrite: bool = typer.Option(False, "--overwrite", "-o", help="If set, replaces any .vit/config.json and timeline.json as if they did not exist. Use at your own caution!")
+):
     """
-        Initializes the current Git repo for vit usage.  
+    Initializes the current Git repo for vit usage.  
     """
 
     try:
-        initConfig()
+        initConfig(overwrite=overwrite)
     except Exception as e:
         typer.secho(f"Failed to initialize vit: {e} -- are you inside a git repository?", fg=typer.colors.RED)
         raise typer.Exit(code=1)
@@ -18,9 +23,34 @@ def init():
 @app.command()
 def test():
     """
-        Test command
+    Test command
     """
     typer.secho(f"Test worked!", fg=typer.colors.GREEN)
+
+@app.command()
+def commit(
+    message: str = typer.Option(..., "-m", help="Commit message"),
+    attach: list[Path] = typer.Option([], "--attach",  exists=True, file_okay=True,  dir_okay=True,  help="Media files to attach, i.e. vit commit -m \"my commit message!\" --attach a.gif b.png c.mp4")
+):
+    """
+    Commits changes, and attaches media files (images, gifs, videos, etc.) to the commit hash
+
+    Should be run after `git add` -- this runs `git commit` under the hood.
+    """
+    # Grab the config paths & media storage metadata
+    config = loadConfig()
+
+    # Actually run git commit
+    # try:
+    #     subprocess.run(f"git commit -m {message}".split(), check=True)
+    # except subprocess.CalledProcessError as e:
+    #     typer.secho(f"Git commit failed: {e}", fg=typer.colors.RED)
+    #     raise typer.Exit(code=1)
+    
+    repo = Repo(config.repoPath)
+    commitHash = repo.head.commit.hexsha[:7]
+    print(f"Commit hash: {commitHash}")
+
 
 if __name__ == "__main__":
     app()
--- vit/config.py
+++ vit/config.py
@@ -1,8 +1,10 @@
 from pathlib import Path
 import typer
 import json
+from types import SimpleNamespace
 
 DEFAULT_CONFIG = {
+    "repoPath" : "",
     "storageDir" : ".vit",
     "mediaSubdir" : "media",
     "timelineFile" : "timeline.json"
@@ -21,31 +23,79 @@ def findGitRoot() -> Path:
         cur = cur.parent
     raise RuntimeError("Could not find a Git repository")
 
-def initConfig():
+def initConfig(overwrite=False):
     """
     1. Search for the git project root
     2. Build the .vit/ directory at that location
     3. Create the config.json with our default config
-    4. Create skeleton for timeline.json, as well as media/ ?
+    4. Create skeleton for timeline.json, as well as media/
     """
 
     # 1)
-    projectRoot = findGitRoot()
+    repoPath = findGitRoot()
 
     # 2)
-    vitDir = projectRoot/DEFAULT_CONFIG["storageDir"]
+    vitDir = repoPath/DEFAULT_CONFIG["storageDir"]
     vitDir.mkdir(exist_ok=True)
 
     # 3)
-    configPath = vitDir/"config.json"
+    configPath = vitDir / "config.json"
+    configToWrite = {
+        **DEFAULT_CONFIG,
+        "repoPath" : str(repoPath)
+    }
+
     if configPath.exists():
-        typer.secho(f"Already initialized at {configPath}, ignoring", fg=typer.colors.YELLOW)
-    else:
-        configPath.write_text(json.dumps(DEFAULT_CONFIG, indent=2))
+        if overwrite:
+            backupConfig = vitDir / "config.json.backup"
+            configPath.replace(backupConfig)
+            typer.secho(f"Overwriting - if this was a mistake, one copy of your timeline.json and config.json are backed up to {backupConfig}!", fg=typer.colors.YELLOW)
+        else:
+            typer.secho(f"Already initialized at {configPath}, ignoring. Add --overwrite to delete your existing timeline data and reinit!", fg=typer.colors.YELLOW)
+            return
+    if not configPath.exists() or overwrite:
+        configPath.write_text(json.dumps(configToWrite, indent=2))
         typer.secho(f"Wrote config to {configPath}", fg=typer.colors.GREEN)
 
     # 4)
-    timelinePath = vitDir/DEFAULT_CONFIG["timelineFile"]
-    if not timelinePath.exists():
+    timelinePath = vitDir / DEFAULT_CONFIG["timelineFile"]
+    if timelinePath.exists():
+        if overwrite:
+            backupTimeline = vitDir / (DEFAULT_CONFIG["timelineFile"] + ".backup")
+            timelinePath.replace(backupTimeline)
+            typer.secho(f"Backed up existing timeline to {backupTimeline}", fg=typer.colors.YELLOW)
+        else:
+            typer.secho(f"Already initialized at {timelinePath}, ignoring. Add --overwrite to delete your existing timeline data and reinit!", fg=typer.colors.YELLOW)
+    if not timelinePath.exists() or overwrite:
         timelinePath.write_text("[]")
         typer.secho(f"Created empty timeline at {timelinePath}", fg=typer.colors.GREEN)
+
+    mediaDir = vitDir / DEFAULT_CONFIG["mediaSubdir"]
+    mediaDir.mkdir(exist_ok=True)
+
+def loadConfig() -> SimpleNamespace:
+    """
+    Loads the relevant project metadata from .vit/config.json
+    """
+    repoPath = findGitRoot()
+    configPath = repoPath / ".vit" / "config.json"
+    if not configPath.exists():
+        typer.secho("Please run `vit init` in the repository first!", fg=typer.colors.RED)
+        raise typer.Exit(code=1)
+    
+    data = json.loads(configPath.read_text())
+
+    for key in DEFAULT_CONFIG.keys():
+        if key not in data:
+            raise Exception(f"Missing key \"{key}\" in config.json")
+    
+    storage = Path(data["storageDir"])
+    if not storage.is_absolute():
+        storage = Path(data["repoPath"]) / storage
+    
+    return SimpleNamespace(
+        repoPath = Path(data["repoPath"]),
+        storageDir = storage,
+        mediaSubdir = data["mediaSubdir"],
+        timelineFile = data["timelineFile"]
+    )
